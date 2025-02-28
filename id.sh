8a4de475798727028cc9d25136b0c02b82344ab9
ZhuZhuZhuang10 <157409639+ZhuZhuZhuang10@users.noreply.github.com>
2025-02-28 19:28:15 +0500

    Create id.sh

---
+++ id.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Запрашиваем ID у пользователя
+echo "Введите ID:"
+read ID
+
+# Выполняем команды
+echo "v2bx"
+echo "15"
+echo "y"
+echo "https://bibihy-shop.org"
+echo "imlalaimlalaimlala"
+echo "y"
+echo "1"
+echo "$ID"
+echo "2"
+echo "y"
+echo "n"
+
+echo "Готово!"
