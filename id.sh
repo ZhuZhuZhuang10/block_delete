6e7930402da0b4c895950c4103ef3e617f7370da
ZhuZhuZhuang10 <157409639+ZhuZhuZhuang10@users.noreply.github.com>
2025-02-28 19:29:31 +0500

    Update id.sh

--- id.sh
+++ id.sh
@@ -4,17 +4,19 @@
 echo "Введите ID:"
 read ID
 
-# Выполняем команды
-echo "v2bx"
-echo "15"
-echo "y"
-echo "https://bibihy-shop.org"
-echo "imlalaimlalaimlala"
-echo "y"
-echo "1"
-echo "$ID"
-echo "2"
-echo "y"
-echo "n"
+# Выполняем команды автоматически
+{
+  echo "v2bx"
+  echo "15"
+  echo "y"
+  echo "https://bibihy-shop.org"
+  echo "imlalaimlalaimlala"
+  echo "y"
+  echo "1"
+  echo "$ID"
+  echo "2"
+  echo "y"
+  echo "n"
+} | some_command
 
 echo "Готово!"
