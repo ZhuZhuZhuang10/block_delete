2c584f680a48edf5f60efd970e9e4bf26e3d69a7
ZhuZhuZhuang10 <157409639+ZhuZhuZhuang10@users.noreply.github.com>
2025-02-28 19:40:19 +0500

    Update and rename id.py to ids.py

--- ids.py
+++ ids.py
@@ -1,9 +1,14 @@
 import time
 import subprocess
+import sys
 
 def main():
-    user_id = input("Введите айди: ")
+    if len(sys.argv) < 2:
+        print("Использование: python3 script.py <айди>")
+        sys.exit(1)
     
+    user_id = sys.argv[1]
+
     commands = [
         "v2bx",
         "15",
@@ -19,8 +24,8 @@ def main():
     ]
     
     for command in commands:
-        subprocess.run(command, shell=True)  # Реальный ввод команды в терминал
-        time.sleep(0.5)  # Небольшая задержка для стабильности
+        subprocess.run(command, shell=True)
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     main()
